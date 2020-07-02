1d1fa18c0042f1a0a40b0297d1c7251be11b93d3
N0vachr0n0 <59119586+N0vachr0n0@users.noreply.github.com>
2020-07-02 10:48:59 +0000

    Update courses_alert.py

--- courses_alert.py
+++ courses_alert.py
@@ -1,20 +1,19 @@
 '''
-Courses Alert version 1.0
-
+Courses Alert version 2.0
 This script look for and alert you about free courses on Techcracked.com and freeonlinecourses.xyz
 with your keywords
-
 By N0vachr0n0
-
 '''
 
 from bs4 import BeautifulSoup
 import requests
 import easygui
 import time
+import webbrowser
 
-titre_cours = []
-new = 0
+titre_cours = [[],[]]
+button = ["CHECK","OK"]
+
 # This is example of keywords
 keywords = ["Android", "IoT", "Ethical", "Machine", "Mobile", "AI", "Python", "Flutter", "Ios", "AWS", "Kali",
             "Complete", "OWASP"]
@@ -22,29 +21,39 @@ keywords = ["Android", "IoT", "Ethical", "Machine", "Mobile", "AI", "Python", "F
 print("Courses Alert is running ...")
 
 
-def cooking(url_, name_class):
+def cooking(url_, name_class, id):
+    global titre_cours
+    new = 0
+    courses = []
     resp = requests.get(url_)
     soup = BeautifulSoup(resp.text, "lxml")
 
     # Checking new courses
-    for art in soup.find_all("h2", class_=name_class, limit=3):
-        if art.get_text not in titre_cours:
-            titre_cours.append(art.get_text())
+    for art in soup.find_all(class_=name_class, limit=3):
+        if art.get_text() not in titre_cours:
+            titre_cours[id].append(art.get_text())
             new = 1
             if titre_cours.__len__() > 4:
-                del (titre_cours[3])
+                del (titre_cours[id][3])
 
     # Verifying whether keywords match with a new course
     if new == 1:
-      for i in range(keywords.__len__()):
-          for y in range(titre_cours.__len__()):
-              if keywords[i - 1] in titre_cours[y - 1]:
-                  easygui.msgbox("{}\nSource:- {}".format(titre_cours[y - 1], url_), title="NEW COURSES !")
-                  new = 0
+        for j in range(titre_cours[id].__len__()):
+            for i in range(keywords.__len__()):
+                if keywords[i] in titre_cours[id][j]:
+                    if titre_cours[id][j] not in courses:
+                        courses.append(titre_cours[id][j])
+        new = 0
 
+
+    for i in range(courses.__len__()):
+        choice = easygui.buttonbox("{}\n\nSource:- {}".format(courses[i], url_), "NEW COURSES !",
+                                   button)
+        if choice == button[0]:
+            webbrowser.open_new("{}".format(url_))
 
 if __name__ == '__main__':
     while 1:
-        cooking("https://www.techcracked.com/", "post-title")
-        cooking("https://freeonlinecourses.xyz/category/hacking/", "entry-title")  # Checking only in Hacking session
+        cooking("https://www.techcracked.com/", "post-title",0)
+        cooking("https://www.freshercooker.in/", "entry-title td-module-title",1)  # Checking only in Hacking session
         time.sleep(14400)  # Waiting 4 hours to check again
